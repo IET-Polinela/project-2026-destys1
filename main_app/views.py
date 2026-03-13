d6cdc663134b6fefaba18d7af0806d30c774294b
destys1 <destysansa@gmail.com>
2026-03-13 22:16:27 +0700

    Lab Session 1: menambahkan kembali halaman /welcome. Sebelumnya Lab Session 1 sudah dibuat dan dipush, namun file manage.py pada project lokal mengalami kerusakan sehingga project harus dibuat ulang. Saat push Lab Session 2 terjadi force push yang menyebabkan commit Lab Session 1 sebelumnya tidak muncul di repository.

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'main_app/home.html')
+
+def welcome(request):
+    return HttpResponse("Selamat Datang")
